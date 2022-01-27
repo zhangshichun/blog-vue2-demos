c79ff823a09bfb1d10cc286456ebced58c4e9d8c
zhangshichun <zhangshichun_looper@aliyun.com>
2022-01-27 13:11:04 +0800

    feat: add upload

--- src/views/test-upload/demo.vue
+++ src/views/test-upload/demo.vue
@@ -8,6 +8,9 @@
       <el-button size="small" type="primary">点击上传</el-button>
       <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
     </el-upload>
+    <div>
+      <el-button @click="upload">模拟上传</el-button>
+    </div>
   </div>
 </template>
 <script>
@@ -16,6 +19,14 @@ export default {
     return {
       fileList: []
     };
+  },
+  methods: {
+    upload() {
+      this.fileList.push({
+        name: 'food3.jpeg',
+        url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+      })
+    }
   },
   mounted() {
     setTimeout(() => {
